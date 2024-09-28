7ffe19dce8fd1914617837721e381340d428e8f0
Robert Kirchner <rjojjr91@live.com>
2024-09-28 16:34:46 -0500

    add logging to python client - rk

--- client-modules/python/src/taskslock/client/tasks_lock_service.py
+++ client-modules/python/src/taskslock/client/tasks_lock_service.py
@@ -2,6 +2,7 @@ from utils import host_utils
 import requests
 import json
 from models.task_lock import TaskLock
+from logging import getLogger
 
 version = '1.0.0'
 
@@ -18,14 +19,23 @@ class TasksLockService:
         self._port = port if not port is None else host_utils.get_port()
         self._url = f'{self._protocol}://{self._host}:{str(self._port)}'
         self.version = version
+        self.logger = getLogger(__name__)
 
     def acquire_lock(self, task_name: str, context_id: str, wait_for_lock: bool) -> TaskLock:
         """Acquires lock from the TasksLockAPI."""
+
+        task_lock = TaskLock({'taskName': task_name, 'contextId': context_id, 'isLockAcquired': False}, lambda : print(f'Cannot release lock for {task_name}, lock not acquired contextId: {context_id}'))
+        self.logger.debug(f"acquiring lock for task {task_name} contextId: {context_id}")
         response = requests.get(f'{self._url}/tasks-lock/api/v1/acquire?taskName={task_name}&contextId={context_id}&waitForLock={"true" if wait_for_lock else "false"}')
         if response.status_code < 300:
             body = json.loads(response.content)
-            return TaskLock(body, lambda: self.release_lock(task_name))
-        return TaskLock({'taskName': task_name, 'contextId': context_id, 'isLockAcquired': False}, lambda : print(f'Cannot release lock for {task_name}, lock not acquired contextId: {context_id}'))
+            task_lock = TaskLock(body, lambda: self.release_lock(task_name))
+        if task_lock.is_locked:
+            self.logger.debug(f"acquired lock for task {task_name} contextId: {context_id}")
+        else:
+            self.logger.warning(f"did not acquire lock for task {task_name} contextId: {context_id}")
+
+        return task_lock
 
     def release_lock(self, task_name: str) -> bool:
         """Releases lock from the TasksLockAPI."""
